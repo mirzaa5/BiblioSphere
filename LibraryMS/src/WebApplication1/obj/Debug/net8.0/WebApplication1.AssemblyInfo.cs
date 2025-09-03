2a6d827ebd968c418967b2dd42b8ea6148b89c71
Mirza Hadi <mirzahadi55@gmail.com>
2025-09-02 20:47:39 -0400

    Minor Update and Bug Fixes

--- LibraryMS/src/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ LibraryMS/src/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39b8f16c3908c2d842ac5c2189736d19d9c6c6b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22794f0b2116ae6c359ca6a086198e2604808633")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
